f8672886bb0180a27e6bb2a5249cb8482773d9d4
Krzysztof Bociurko <git@chanibal.pl>
2018-07-26 15:45:06 +0200

    Test case for double click compound button bug

---
+++ Assets/Button-bug-test-case/ButtonClickHandler.cs
@@ -0,0 +1,23 @@
+﻿using HoloToolkit.Unity.Buttons;
+using UnityEngine;
+
+public class ButtonClickHandler : MonoBehaviour {
+
+	public UnityEngine.UI.Text text;
+
+	private void OnEnable()
+	{
+		GetComponent<Button>().OnButtonClicked += ButtonClickHandler_OnButtonClicked;
+	}
+
+	private void OnDisable()
+	{
+		GetComponent<Button>().OnButtonClicked -= ButtonClickHandler_OnButtonClicked;
+	}
+
+	private void ButtonClickHandler_OnButtonClicked(GameObject obj)
+	{
+		Debug.LogFormat(this, "Clicked obj={0} current={1} t={2}", obj, Event.current, Time.realtimeSinceStartup);
+		text.text += string.Format("\n\n\nClicked obj={0} current={1} t={2}\n{3}", obj, Event.current, Time.realtimeSinceStartup, System.Environment.StackTrace);
+	}
+}
